509c17ab19c7c371005d414fd63c07abf120e793
Rudolf Sailer <Rudolf.Sailer@mercateo.com>
2018-06-29 11:38:20 +0200

    fixes for 0.7

--- src/active_set.jl
+++ src/active_set.jl
@@ -1,4 +1,4 @@
-immutable ActiveState
+struct ActiveState
     weighted_label::Float64
     weight::Float64
     lower::Int64
@@ -48,14 +48,14 @@ function active_set_isotonic_regression!(y::Vector{Float64}, weights::Vector{Flo
         end
 
         for as in active_set
-            y[as.lower:as.upper] = as.weighted_label / as.weight
+            y[as.lower:as.upper] .= as.weighted_label / as.weight
         end
     end
     return y
 end
 
-active_set_isotonic_regression!(y::Vector{Float64}) = active_set_isotonic_regression!(y, ones(size(y, 1)))
+active_set_isotonic_regression!(y::Vector{Float64}) = active_set_isotonic_regression!(y, fill!(similar(y), one(eltype(y))))
 
 # non-mutating versions
 active_set_isotonic_regression(y::Vector{Float64}, weights::Vector{Float64}) = active_set_isotonic_regression!(copy(y), weights)
-active_set_isotonic_regression(y::Vector{Float64}) = active_set_isotonic_regression(y, ones(size(y,1)))
+active_set_isotonic_regression(y::Vector{Float64}) = active_set_isotonic_regression(y, fill!(similar(y), one(eltype(y))))
