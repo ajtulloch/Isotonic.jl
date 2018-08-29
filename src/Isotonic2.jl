ba69bb5527a5392995cc9a9fb1e5f8bfed847928
Rudolf Sailer <Rudolf.Sailer@mercateo.com>
2018-08-29 16:46:18 +0200

    name change

---
+++ src/Isotonic2.jl
@@ -0,0 +1,14 @@
+module Isotonic2
+
+export isotonic_regression,
+       isotonic_regression!,
+       active_set_isotonic_regression,
+       active_set_isotonic_regression!,
+       pooled_pava_isotonic_regression,
+       pooled_pava_isotonic_regression!
+
+include("linear_pava.jl")
+include("active_set.jl")
+include("pooled_pava.jl")
+
+end
